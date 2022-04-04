3776dbca81560867cbf13f982d2e9dd463fbe296
Nafees <abdur17@student.sust.edu>
2022-04-05 05:20:01 +0600

    active route added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Header.css'
-import { Link, NavLink, Route, Routes } from 'react-router-dom';
-// import Reviews from '../Reviews/Reviews';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='m-3 '>
+        <div className='m-4'>
 
-            <NavLink  className='mr-3 hover:text-blue-600 hover:font-bold' to="/">Home</NavLink>
+            <NavLink  className='mr-3 text-lg active' to="/">Home</NavLink>
 
-            <NavLink  className='mr-3 hover:text-blue-600 hover:font-bold' to="/reviews">Reviews</NavLink>
+            <NavLink  className='mr-3 text-lg active' to="/reviews" >Reviews</NavLink>
 
-            <NavLink className=' mr-3 hover:text-blue-600 active:font-bold hover:font-bold'  to="/dashboard">Dashboard</NavLink>
+            <NavLink className='mr-3 active text-lg'  to="/dashboard" >Dashboard</NavLink>
 
-            <NavLink  className='mr-3 hover:text-blue-600 hover:font-bold' to="/blogs">Blogs</NavLink>
+            <NavLink  className='active mr-3 text-lg' to="/blogs">Blogs</NavLink>
 
-            <NavLink className='mr-3 hover:text-blue-600 hover:font-bold' to="/about">About</NavLink>
+            <NavLink className='active mr-3 text-lg' to="/about">About</NavLink>
         </div>
     );
 };
